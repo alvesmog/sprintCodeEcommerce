89d1113e617c08c62cfb6de019ae630c6f4b5f90
Elton Dornelas <eltondornelas@gmail.com>
2020-03-13 19:48:22 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/spring/code/ecommerce/App.java
@@ -0,0 +1,13 @@
+package br.com.spring.code.ecommerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
