2d5ddbf82b4f905769a5fdf2e45b0db3cb49d5ff
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-13 21:33:00 -0300

    [novo] Categoria

---
+++ src/main/java/br/com/spring/code/ecommerce/gestaoprodutos/Categoria.java
@@ -0,0 +1,28 @@
+package br.com.spring.code.ecommerce.gestaoprodutos;
+
+public class Categoria {
+	
+	// Atributos
+	
+	private Integer id; 
+	private String nome;
+	
+	// Setters e Getters
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
