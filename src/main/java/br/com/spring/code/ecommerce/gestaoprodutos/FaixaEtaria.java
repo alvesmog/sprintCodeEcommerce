e5d5034e3d0b00f93d81ebe30bb2fad500dc7c7f
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-14 09:54:14 -0300

    [fix] mudança das categorias

--- src/main/java/br/com/spring/code/ecommerce/gestaoprodutos/FaixaEtaria.java
+++ src/main/java/br/com/spring/code/ecommerce/gestaoprodutos/FaixaEtaria.java
@@ -2,10 +2,17 @@ package br.com.spring.code.ecommerce.gestaoprodutos;
 
 public enum FaixaEtaria {
 	
-	BABY_PP,
-	BABY_P,
-	BABY_M,
-	BABY_G,
-	BABY_GG,
+	// Anos
+	
+	ZERO_A_DOIS,
+	DOIS_A_QUATRO,
+	QUATRO_A_SEIS,
+	SEIS_A_OITO,
+	
+	// Meses
+	ZERO_A_TRES_MESES,
+	TRES_A_SEIS_MESES,
+	SEIS_A_DOZE_MESES,
+	DOZE_A_DEZOITO_MESES,
 
 }
