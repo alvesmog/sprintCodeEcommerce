6aa008b2c744b75f2ebe7784345c7a0ca7107b6e
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-04-12 10:43:09 -0300

    [mudança] Pagamento virou apenas uma lista de opções de pagamento

--- src/main/java/br/com/spring/code/ecommerce/financeiro/Pagamento.java
+++ src/main/java/br/com/spring/code/ecommerce/financeiro/Pagamento.java
@@ -1,30 +1,10 @@
 package br.com.spring.code.ecommerce.financeiro;
 
-public class Pagamento {
-	private int Id;
-	private String formaPagamento;
+public enum Pagamento {
 	
-	public Pagamento(int id, String formaPagamento) {
-		super();
-		Id = id;
-		this.formaPagamento = formaPagamento;
-	}
-
-	public int getId() {
-		return Id;
-	}
-
-	public void setId(int id) {
-		Id = id;
-	}
-
-	public String getFormaPagamento() {
-		return formaPagamento;
-	}
-
-	public void setFormaPagamento(String formaPagamento) {
-		this.formaPagamento = formaPagamento;
-	}
-	
+	BOLETO,
+	CARTAO_DE_CREDITO,
+	DEBITO,
+	PAYPAL
 	
 }
