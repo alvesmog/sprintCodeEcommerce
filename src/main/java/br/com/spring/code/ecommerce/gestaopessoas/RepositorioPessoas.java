0a30562fc7d401c5c8d0821e4ebb2c407fdb3ae2
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-04-18 11:15:00 -0300

    [melhoria] autenticação e verificação de permissões

--- src/main/java/br/com/spring/code/ecommerce/gestaopessoas/RepositorioPessoas.java
+++ src/main/java/br/com/spring/code/ecommerce/gestaopessoas/RepositorioPessoas.java
@@ -80,5 +80,70 @@ public class RepositorioPessoas {
 		System.out.println("Pessoa com esse ID não encontrada.");
 
 	}
+	
+	/**
+	 * Autentica se o usuario está no repositório.
+	 * @param login
+	 * @param senha
+	 * @return {@code true} se está autenticado, {@code false} caso contrário.
+	 */
+	public boolean estaAutenticado(String login, String senha) {
+		
+		for (Pessoa pessoa : listaDePessoas) {
+			
+			// Verica se existe o usuario com aquele `login`
+			if (pessoa.getUsuario().getLogin().equals(login)) {
+				
+				// Caso exista, verifica se a `senha` procede:
+				if (pessoa.getUsuario().getSenha().equals(senha))
+					
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	 * Autentica o usuario e retorna o objeto {@code Pessoa} associado.
+	 * @param login
+	 * @param senha
+	 * @return {@code Pessoa} pessoaAutenticada OR {@code null}
+	 */
+	public Pessoa estaAutenticadoRetornaPessoa(String login, String senha) {
+		
+		for (Pessoa pessoa : listaDePessoas) {
+			
+			// Verica se existe o usuario com aquele `login`
+			if (pessoa.getUsuario().getLogin().equals(login)) {
+				
+				// Caso exista, verifica se a `senha` procede:
+				if (pessoa.getUsuario().getSenha().equals(senha))
+					
+				return pessoa;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Verifica o tipo de usuario, com base no 
+	 * {@code login} e {@code senha} informados.
+	 * @return 
+	 * <p> -1 -> caso não haja permissão; </p>
+	 * <p> 1 -> caso seja Pessoa Jurídica; </p>
+	 * <p> 2 -> caso seja Pessoa Física; </p>
+	 * 
+	 */
+	public int verificarPermissao(String login, String senha) {
+		
+		Pessoa pessoaProcurada = this.estaAutenticadoRetornaPessoa(login, senha);
+		
+		if (pessoaProcurada.getPessoa() instanceof PessoaJuridica) {
+			return 1;
+		} else if (pessoaProcurada.getPessoa() instanceof PessoaFisica) {
+			return 2;
+		}
+		
+		return -1;
+	}
 
 }
