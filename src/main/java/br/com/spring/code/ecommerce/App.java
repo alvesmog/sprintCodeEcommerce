0111452d1cb71c069cf8bf79e8f063bdfcf1e185
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-04-18 14:11:05 -0300

    [melhoria] implementação da Interface principal
     menuGeral();
    
    subMenuPessoas(), etc ...

--- src/main/java/br/com/spring/code/ecommerce/App.java
+++ src/main/java/br/com/spring/code/ecommerce/App.java
@@ -16,7 +16,8 @@ import br.com.spring.code.ecommerce.gestaoprodutos.RepositorioProdutos;
  */
 public class App 
 {
-    public static void main( String[] args )
+    // [fix] source: Aula 18/04,  9-12h: https://github.com/alvesmog/sprintCodeEcommerce/commit/b069ece92b571a188769eec075737660e8a44743
+    public static void main( String[] args ) throws InterruptedException
     {
         //Populando dados no sistema
     	RepositorioPessoas pessoas = new RepositorioPessoas();
@@ -29,16 +30,21 @@ public class App
     	Endereco end1 = new Endereco(1, "Rua A", "125", "Cordeiro", "A", "A", "A", "Z", "A", "D");
     	Endereco end2 = new Endereco(2, "Rua 12", "25", "Torre", "A", "C", "A", "Z", "A", "D");
     	
-    	Usuario user1 = new Usuario(1, "joao", "1234");
-    	Usuario user2 = new Usuario(2, "arthur", "4321");
+    	Usuario user1 = new Usuario(1, "user1", "1234");
+     	Usuario user2 = new Usuario(2, "user2", "4321");
+     	Usuario user3 = new Usuario(3, "user3", "4321");
+     	Usuario user4 = new Usuario(4, "user4", "4321");
+     	Usuario user5 = new Usuario(5, "user5", "4321");
+     	Usuario user6 = new Usuario(6, "user6", "4321");
+     	Usuario user7 = new Usuario(7, "user7", "4321");    	
     	
     	Pessoa pessoa1 = new Pessoa("Joao Augusto", "986413568", "joao@teste.com", 1.5, ((TipoPessoa) pf), end1, user1, 2);
     	Pessoa pessoa2 = new Pessoa("Arthur Leandro", "96853214", "arthur@teste.com", 2.5, ((TipoPessoa) pj), end2, user2, 3);
-    	Pessoa pessoa3 = new Pessoa("Carlina", "5842027547", "candryushchenko0@a8.net", 1.0, ((TipoPessoa) pj), end2, user2, 3);
-    	Pessoa pessoa4 = new Pessoa("Nichole", "8232611354", "nbrigdale1@vkontakte.ru", 4.2, ((TipoPessoa) pj), end2, user2, 3);
-    	Pessoa pessoa5 = new Pessoa("Matthew", "2555717124", "mtitmuss2@multiply.com", 3.5, ((TipoPessoa) pj), end2, user2, 3);
-    	Pessoa pessoa6 = new Pessoa("Corrinne", "7047897644", "cchallens3@1688.com", 1.8, ((TipoPessoa) pj), end2, user2, 3);
-    	Pessoa pessoa7 = new Pessoa("Sherilyn", "9461924214", "sivanyutin4@sakura.ne.jp", 2.2, ((TipoPessoa) pj), end2, user2, 3);
+    	Pessoa pessoa3 = new Pessoa("Carlina Perez", "5842027547", "candryushchenko0@a8.net", 1.0, ((TipoPessoa) pj), end2, user3, 3);
+    	Pessoa pessoa4 = new Pessoa("Nichole Lima", "8232611354", "nbrigdale1@vkontakte.ru", 4.2, ((TipoPessoa) pf), end2, user4, 3);
+    	Pessoa pessoa5 = new Pessoa("Matthew Souza", "2555717124", "mtitmuss2@multiply.com", 3.5, ((TipoPessoa) pj), end2, user5, 3);
+    	Pessoa pessoa6 = new Pessoa("Corrinne Silva", "7047897644", "cchallens3@1688.com", 1.8, ((TipoPessoa) pj), end2, user6, 3);
+    	Pessoa pessoa7 = new Pessoa("Sherilyn Perez", "9461924214", "sivanyutin4@sakura.ne.jp", 2.2, ((TipoPessoa) pj), end2, user7, 3);
     	
     	pessoas.adicionar(pessoa1);
     	pessoas.adicionar(pessoa2);
@@ -49,9 +55,9 @@ public class App
     	pessoas.adicionar(pessoa7);
     	
     	Categoria cat1 = new Categoria(1, "Roupa");
-    	Categoria cat2 = new Categoria(1, "Móveis");
-    	Categoria cat3 = new Categoria(1, "Decoração");
-    	Categoria cat4 = new Categoria(1, "Peças de Cama");
+    	Categoria cat2 = new Categoria(2, "Móveis");
+    	Categoria cat3 = new Categoria(3, "Decoração");
+    	Categoria cat4 = new Categoria(4, "Peças de Cama");
     	
     	Produto produto1 = new Produto(1, "Ninho para Bebê Redutor", "Com a suavidade do tecido 100% algodão e enchimento fofinho", "./images/ninho.png", pessoa1);
     	Produto produto2 = new Produto(2, "Berço Minicama 3 em 1 Dom", "Com design moderno, a cabeceira e a peseira se destacam com originais linhas curvilíneas", "./images/berço.png", pessoa2);
@@ -67,7 +73,39 @@ public class App
     	produtos.adicionar(produto5);
     	produtos.adicionar(produto6);
     	
+        // [fix] source: Aula 18/04,  9-12h: https://github.com/alvesmog/sprintCodeEcommerce/commit/b069ece92b571a188769eec075737660e8a44743
+    	InterfaceDoApp menuGeral = new InterfaceDoApp();
     	
-    	
+        //Faz a chamada do menu de opções
+
+      	InterfaceSubMenuPessoas menuPessoas = new InterfaceSubMenuPessoas();
+     	int rep;
+     	do {
+     		rep = menuGeral.mostrarMenuOpcoes();
+
+      		switch (rep) {
+ 			case 1 :  int op;
+ 					  do {
+ 						op= menuPessoas.mostrarSubMenuPessoas();
+ 						menuPessoas.ingressaOpcoesGestaoPessoa(op, pessoas);
+ 					  } while (op != 0);
+ 				break; 
+ 			case 2 : //chamar a classe/metodo do submenu de gestão de produtos 
+ 				break;
+ 			case 3 : //chamar a classe/metodo do submenu de gestão de anuncio 
+ 				break;
+ 			case 4 : //chamar a classe/metodo do submenu de gestão de vendas 
+ 				break;
+ 			case 5 : //chamar a classe/metodo do submenu de gestão de atendimento 
+ 				break;
+
+  			case 0 : System.out.println("Saindo do Sistema... ");
+ 					Thread.sleep(3000);
+ 			break;
+
+  			default: System.out.println("Opção Inválida");
+ 		}
+
+      	} while (rep != 0);
     }
 }
