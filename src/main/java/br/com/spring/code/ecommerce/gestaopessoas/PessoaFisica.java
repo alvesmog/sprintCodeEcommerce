f9730237fbe998866082decbef75cda5f61c816a
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-04-18 09:50:20 -0300

    [melhoria] construtor da classa + toString()

--- src/main/java/br/com/spring/code/ecommerce/gestaopessoas/PessoaFisica.java
+++ src/main/java/br/com/spring/code/ecommerce/gestaopessoas/PessoaFisica.java
@@ -2,6 +2,11 @@ package br.com.spring.code.ecommerce.gestaopessoas;
 
 public class PessoaFisica extends TipoPessoa {
 	
+	// [fix] source: https://github.com/alvesmog/sprintCodeEcommerce/commit/4aa4cde652ed03a8c2f3b81b59eed7154715f05e
+	public PessoaFisica(String cpf) {
+ 		this.cpf = cpf;
+ 	}
+	
 	private String cpf;
 
 	public String getCpf() {
@@ -11,5 +16,10 @@ public class PessoaFisica extends TipoPessoa {
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}	
-
+	
+	// [fix] source: https://github.com/alvesmog/sprintCodeEcommerce/commit/4aa4cde652ed03a8c2f3b81b59eed7154715f05e
+	@Override
+ 	public String toString() {
+ 		return "PessoaFisica [Cpf()=" + getCpf() + "]";
+ 	}
 }
