857b2c775d68b87aa46deb921c0d5814191a8551
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-04-18 09:52:41 -0300

    [melhoria] construtor da classe + toString() +…

--- src/main/java/br/com/spring/code/ecommerce/gestaopessoas/PessoaJuridica.java
+++ src/main/java/br/com/spring/code/ecommerce/gestaopessoas/PessoaJuridica.java
@@ -3,13 +3,24 @@ package br.com.spring.code.ecommerce.gestaopessoas;
 public class PessoaJuridica extends TipoPessoa {
 	
 	private String cnpj;
+	
+	// [fix] source: https://github.com/alvesmog/sprintCodeEcommerce/commit/4aa4cde652ed03a8c2f3b81b59eed7154715f05e
+	public PessoaJuridica(String cnpj) {
+ 		this.cnpj = cnpj;
+ 	}
 
 	public String getCnpj() {
 		return cnpj;
 	}
-
-	public void setCpf(String cnpj) {
+	
+	// [fix] nome do método estava "setCpf()"
+	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}	
-
+	
+	// [fix] source: https://github.com/alvesmog/sprintCodeEcommerce/commit/4aa4cde652ed03a8c2f3b81b59eed7154715f05e
+	@Override
+ 	public String toString() {
+ 		return "PessoaJuridica [CNPJ = " + getCnpj() + " ]";
+ 	}
 }
