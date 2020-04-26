ac297eeed2bdfecb3e2f5850a91428eaf3c773e5
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-04-25 22:41:28 -0300

    [fix] fazendo um toString() mais robusto

--- src/main/java/br/com/spring/code/ecommerce/gestaoprodutos/Produto.java
+++ src/main/java/br/com/spring/code/ecommerce/gestaoprodutos/Produto.java
@@ -135,13 +135,24 @@ public class Produto {
 			return false;
 		return true;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "Produto [titulo=" + titulo 
-				+ ", Categoria=" + categoria.toString() 
-				+ ", Estado=" + estado 
-				+ ", FaixaEtaria=" + faixaEtaria + "]\n";
+		StringBuilder builder = new StringBuilder();
+		builder.append("Produto [ ID =");
+		builder.append(getId());
+		builder.append(", Titulo =");
+		builder.append(getTitulo());
+		builder.append(", Categoria =");
+		builder.append(getCategoria());
+		builder.append(", Estado =");
+		builder.append(getEstado());
+		builder.append(", FaixaEtaria =");
+		builder.append(getFaixaEtaria());
+		builder.append(", ID Vendedor ()=");
+		builder.append(getIdPessoa());
+		builder.append("]");
+		return builder.toString();
 	}
-	
+
 }
