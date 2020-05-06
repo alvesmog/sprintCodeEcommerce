92439dda63c3bdf1d5c375dffced126fb19763de
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-05 23:38:42 -0300

    [melhoria] implementa os métodos de procura na Interface

--- src/main/java/br/com/spring/code/ecommerce/venda/RepositorioVendas.java
+++ src/main/java/br/com/spring/code/ecommerce/venda/RepositorioVendas.java
@@ -2,10 +2,14 @@ package br.com.spring.code.ecommerce.venda;
 
 import java.util.*;
 
+import br.com.spring.code.ecommerce.gestaopessoas.Pessoa;
+
 public interface RepositorioVendas {
 
 	void registrarVenda(Venda venda);
 	List<Venda> listarVendas();
 	Venda procurarVenda(Date data);
 	
+	public List<Venda> procurarVendasPorComprador(Pessoa Comprador);
+	public List<Venda> procurarVendasPorVendedor(Pessoa Vendedor);
 }
