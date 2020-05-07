f0f1d12445c5815a2b57e976fe5c327b4ea02bca
Heber Ribeiro <heber.r@outlook.com>
2020-05-07 14:28:41 -0300

    [fix] Adicionar e remover RepositorioPessoas

--- src/main/java/br/com/spring/code/ecommerce/gestaopessoas/RepositorioPessoas.java
+++ src/main/java/br/com/spring/code/ecommerce/gestaopessoas/RepositorioPessoas.java
@@ -12,120 +12,133 @@ import java.util.List;
  */
 
 public class RepositorioPessoas {
-	
-	// [fix] source: https://github.com/alvesmog/sprintCodeEcommerce/commit/4aa4cde652ed03a8c2f3b81b59eed7154715f05e
-	private List<Pessoa> listaDePessoas = new ArrayList<Pessoa>();
+
+	private static List<Pessoa> listaDePessoas = new ArrayList<Pessoa>();
+	private static int idReferencia;
 
 	public RepositorioPessoas() {
-		
+
 	}
 
 	public void exibirTodasPessoas() {
 		for (int i = 0; i < listaDePessoas.size(); i++) {
-			System.out.println(listaDePessoas.get(i));			
+			System.out.println(listaDePessoas.get(i));
 		}
 	}
-	
-	public void exibir(Integer id) {	
+
+	public void exibir(Integer id) {
 		System.out.println(listaDePessoas.get(id - 1));
 	}
 
-	public void adicionar(Pessoa pessoa) {		
-		listaDePessoas.add(pessoa);		
-		for (int i = 0; i < listaDePessoas.size(); i++) {			
-			if (listaDePessoas.get(i) == pessoa) {				
-				listaDePessoas.get(i).setId(i + 1);
-				listaDePessoas.get(i).getUsuario().setId(i + 1);
-				listaDePessoas.get(i).getEndereco().setId(i + 1);				
-			}			
-		}		
-	}	
-
-	public void remover(Integer idParaRemover) {		
-		if (listaDePessoas.get(idParaRemover - 1) != null) {
-			listaDePessoas.remove(listaDePessoas.get(idParaRemover -1));
+	public void adicionar(Pessoa pessoa) {
+		if (listaDePessoas.isEmpty()) {
+			pessoa.setId(1);
+			listaDePessoas.add(pessoa);
+			idReferencia = 1;
 		} else {
-			System.out.println("Pessoa com esse ID não encontrada.");
+			idReferencia = idReferencia + 1;
+			pessoa.setId(idReferencia);
+			listaDePessoas.add(pessoa);
+		}
+
+	}
+
+	public void remover(Integer idParaRemover) {
+
+		for (int i = 0; i < listaDePessoas.size(); i++) {
+			if (listaDePessoas.get(i).getId() == idParaRemover) {
+				listaDePessoas.remove(i);
+			}
 		}
 	}
-	
-	//VALIDAR 
-	
-	public String validaCpfCnpj(String entrada) {		
-		int tamanho = entrada.length();			
-		if (tamanho == 11) {			
-			return "CPF";			
+
+	// VALIDAR
+
+	public String validaCpfCnpj(String entrada) {
+		int tamanho = entrada.length();
+		if (tamanho == 11) {
+			return "CPF";
 		} else {
-			if (tamanho == 14) {				
-				return "CNPJ";				
-			} else {				
-				return null;				
-			}					
-		}		
-	}	
-	
+			if (tamanho == 14) {
+				return "CNPJ";
+			} else {
+				return null;
+			}
+		}
+	}
+
 	/**
 	 * Autentica se o usuario está no repositório.
+	 * 
 	 * @param login
 	 * @param senha
 	 * @return {@code true} se está autenticado, {@code false} caso contrário.
 	 */
 	public boolean estaAutenticado(String login, String senha) {
-		
-		for (Pessoa pessoa : listaDePessoas) {			
-			
+
+		for (Pessoa pessoa : listaDePessoas) {
+
 			// Verica se existe o usuario com aquele `login`
 			if (pessoa.getUsuario().getLogin().equals(login)) {
-				
+
 				// Caso exista, verifica se a `senha` procede:
 				if (pessoa.getUsuario().getSenha().equals(senha))
-					
-				return true;
+
+					return true;
 			}
 		}
 		return false;
 	}
+
 	/**
 	 * Autentica o usuario e retorna o objeto {@code Pessoa} associado.
+	 * 
 	 * @param login
 	 * @param senha
 	 * @return {@code Pessoa} pessoaAutenticada OR {@code null}
 	 */
 	public Pessoa estaAutenticadoRetornaPessoa(String login, String senha) {
-		
+
 		for (Pessoa pessoa : listaDePessoas) {
-			
+
 			// Verica se existe o usuario com aquele `login`
 			if (pessoa.getUsuario().getLogin().equals(login)) {
-				
+
 				// Caso exista, verifica se a `senha` procede:
 				if (pessoa.getUsuario().getSenha().equals(senha))
-					
-				return pessoa;
+
+					return pessoa;
 			}
 		}
 		return null;
 	}
-	
+
 	/**
-	 * Verifica o tipo de usuario, com base no 
-	 * {@code login} e {@code senha} informados.
-	 * @return 
-	 * <p> -1 -> caso não haja permissão; </p>
-	 * <p> 1 -> caso seja Pessoa Jurídica; </p>
-	 * <p> 2 -> caso seja Pessoa Física; </p>
+	 * Verifica o tipo de usuario, com base no {@code login} e {@code senha}
+	 * informados.
+	 * 
+	 * @return
+	 *         <p>
+	 *         -1 -> caso não haja permissão;
+	 *         </p>
+	 *         <p>
+	 *         1 -> caso seja Pessoa Jurídica;
+	 *         </p>
+	 *         <p>
+	 *         2 -> caso seja Pessoa Física;
+	 *         </p>
 	 * 
 	 */
 	public int verificarPermissao(String login, String senha) {
-		
-		Pessoa pessoaProcurada = this.estaAutenticadoRetornaPessoa(login, senha);		
-		
+
+		Pessoa pessoaProcurada = this.estaAutenticadoRetornaPessoa(login, senha);
+
 		if (pessoaProcurada instanceof PessoaJuridica) {
 			return 1;
 		} else if (pessoaProcurada instanceof PessoaFisica) {
 			return 2;
 		}
-		
+
 		return -1;
 	}
 
