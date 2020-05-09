b9031f7736daa096edf160cdef2d45c6d2f5e74e
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-09 18:47:55 -0300

    [fix] identação + instancia dos repositórios

--- src/main/java/br/com/spring/code/ecommerce/App.java
+++ src/main/java/br/com/spring/code/ecommerce/App.java
@@ -37,24 +37,28 @@ import br.com.spring.code.ecommerce.venda.RepositorioVendas;
 
 public class App 
 {
-    // [fix] source: Aula 18/04,  9-12h: https://github.com/alvesmog/sprintCodeEcommerce/commit/b069ece92b571a188769eec075737660e8a44743
-    public static void main( String[] args ) throws InterruptedException
-    {
-        //Populando dados no sistema
-   	RepositorioPessoas pessoas = new RepositorioPessoas();
-   	RepositorioProdutos produtos = new RepositorioProdutos();	
-    RepositorioCategorias categorias = new RepositorioCategorias();
+	// [fix] source: Aula 18/04,  9-12h: https://github.com/alvesmog/sprintCodeEcommerce/commit/b069ece92b571a188769eec075737660e8a44743
+	public static void main( String[] args ) throws InterruptedException
+	{
+		//Populando dados no sistema
+		RepositorioPessoas pessoas = new RepositorioPessoas();
+		RepositorioProdutos produtos = new RepositorioProdutos();	
+		RepositorioCategorias categorias = new RepositorioCategorias();
+		RepositorioAnuncio anuncios = new ListaRepositorioAnuncio();
+		RepositorioFinanceiro financas = new ListaRepositorioFinanceiro();
+		RepositorioVendas vendas = new ListaRepositorioVendas();
+		ControladorSessao controladorSessao = new ControladorSessao();
 
-    	//[UPDATE] Redefinidos os instanciamentos seguindo as mudanças na estrutura do pacote "gestaopessoas" | INICIO
+		//[UPDATE] Redefinidos os instanciamentos seguindo as mudanças na estrutura do pacote "gestaopessoas" | INICIO
 
-    Endereco end1 = new Endereco("50010-060", "105", "Banco Itaú", "Brasil");
-   	Endereco end2 = new Endereco("50010-230", "1212", "", "Brasil");
-   	Endereco end3 = new Endereco("50020-040", "06", "Nenhum", "Brasil");
-   	Endereco end4 = new Endereco("50010-080", "591", "", "Brasil");
-   	Endereco end5 = new Endereco("50050-130", "13", "Casa de festas", "Brasil");
-   	Endereco end6 = new Endereco("50050-070", "198", "", "Brasil");    			
-    Endereco end7 = new Endereco("50040-220", "01", "Proximo ao colégio e curso XYZ", "Brasil");
-    	    	    	
+		Endereco end1 = new Endereco("50010-060", "105", "Banco Itaú", "Brasil");
+		Endereco end2 = new Endereco("50010-230", "1212", "", "Brasil");
+		Endereco end3 = new Endereco("50020-040", "06", "Nenhum", "Brasil");
+		Endereco end4 = new Endereco("50010-080", "591", "", "Brasil");
+		Endereco end5 = new Endereco("50050-130", "13", "Casa de festas", "Brasil");
+		Endereco end6 = new Endereco("50050-070", "198", "", "Brasil");    			
+		Endereco end7 = new Endereco("50040-220", "01", "Proximo ao colégio e curso XYZ", "Brasil");
+
 		Usuario user1 = new Usuario("user1", "1234");
 		Usuario user2 = new Usuario("user2", "2020");
 		Usuario user3 = new Usuario("user3", "1111");
@@ -75,7 +79,7 @@ public class App
 				"60054463000168", "Informatica SA");
 		Pessoa pessoa7 = new PessoaJuridica("Sherilyn Perez", "9461924214", "sivanyutin4@sakura.ne.jp", end7, user7,
 				"37500473000168", "Cosmeticos Ltda");
-		
+
 		pessoas.adicionar(pessoa1);
 		pessoas.adicionar(pessoa2);
 		pessoas.adicionar(pessoa3);
@@ -87,23 +91,23 @@ public class App
 		// pacote "gestaopessoas" | FINAL
 
 		Categoria cat1 = new Categoria(1, "Roupa");
-   	Categoria cat2 = new Categoria(2, "Móveis");
-   	Categoria cat3 = new Categoria(3, "Decoração");
-   	Categoria cat4 = new Categoria(4, "Peças de Cama");
-    Categoria cat5 = new Categoria(5, "Brinquedo");
-    
-    categorias.adicionarCategoria(cat1);
-   	categorias.adicionarCategoria(cat2);
-   	categorias.adicionarCategoria(cat3);
-   	categorias.adicionarCategoria(cat4);
-   	categorias.adicionarCategoria(cat5);
-    	
-   	Produto produto1 = new Produto(1, "Ninho para Bebê Redutor", "Com a suavidade do tecido 100% algodão e enchimento fofinho", "./images/ninho.png", pessoa1,cat1);    	Produto produto2 = new Produto(2, "Berço Minicama 3 em 1 Dom", "Com design moderno, a cabeceira e a peseira se destacam com originais linhas curvilíneas", "./images/berço.png", pessoa2,cat2);
-   	Produto produto3 = new Produto(3, "Andador Goal Walker 4 em 1 Cosco", "Com a suavidade do tecido 100% algodão e enchimento fofinho", "./images/andardor.png", pessoa3, cat2);
-   	Produto produto4 = new Produto(4, "Prateleira com Varão Branca", "madeira MDF com varão é a escolha ideal para acomodar caixas e enfeites", "./images/prateleira.png", pessoa4, cat3);
-   	Produto produto5 = new Produto(5, "Kit Mijões Bebê 3 Peças Vaquinha", "Confeccionadas em 85% algodão e 15% pode variar entre outras fibras", "./images/kitmijoes.png", pessoa5,cat4);
-   	Produto produto6 = new Produto(6, "Kit Berço Trança Poá Azul Bebê", "com peças confeccionadas em tecidos 100% algodão e essenciais para o enxoval do bebê", "./images/kitberco.png", pessoa6,cat5);
-    Produto produto7 = new Produto(7,"Camisa","Camisa Azul","Foto",pessoa7,cat5);
+		Categoria cat2 = new Categoria(2, "Móveis");
+		Categoria cat3 = new Categoria(3, "Decoração");
+		Categoria cat4 = new Categoria(4, "Peças de Cama");
+		Categoria cat5 = new Categoria(5, "Brinquedo");
+
+		categorias.adicionarCategoria(cat1);
+		categorias.adicionarCategoria(cat2);
+		categorias.adicionarCategoria(cat3);
+		categorias.adicionarCategoria(cat4);
+		categorias.adicionarCategoria(cat5);
+
+		Produto produto1 = new Produto(1, "Ninho para Bebê Redutor", "Com a suavidade do tecido 100% algodão e enchimento fofinho", "./images/ninho.png", pessoa1,cat1);    	Produto produto2 = new Produto(2, "Berço Minicama 3 em 1 Dom", "Com design moderno, a cabeceira e a peseira se destacam com originais linhas curvilíneas", "./images/berço.png", pessoa2,cat2);
+		Produto produto3 = new Produto(3, "Andador Goal Walker 4 em 1 Cosco", "Com a suavidade do tecido 100% algodão e enchimento fofinho", "./images/andardor.png", pessoa3, cat2);
+		Produto produto4 = new Produto(4, "Prateleira com Varão Branca", "madeira MDF com varão é a escolha ideal para acomodar caixas e enfeites", "./images/prateleira.png", pessoa4, cat3);
+		Produto produto5 = new Produto(5, "Kit Mijões Bebê 3 Peças Vaquinha", "Confeccionadas em 85% algodão e 15% pode variar entre outras fibras", "./images/kitmijoes.png", pessoa5,cat4);
+		Produto produto6 = new Produto(6, "Kit Berço Trança Poá Azul Bebê", "com peças confeccionadas em tecidos 100% algodão e essenciais para o enxoval do bebê", "./images/kitberco.png", pessoa6,cat5);
+		Produto produto7 = new Produto(7,"Camisa","Camisa Azul","Foto",pessoa7,cat5);
 
 		produtos.adicionar(produto1);
 		produtos.adicionar(produto2);
@@ -112,30 +116,30 @@ public class App
 		produtos.adicionar(produto5);
 		produtos.adicionar(produto6);
 
-    	/**
-    	 * 	ANÚNCIO:
-    	 * 
-    	 *  - [] Criar anúncios: é preciso criar Datas, Geolocalização, Dúvidas
-    	 *  
-    	 */
-    	
-   	GeoLocalizacao geo1 = new GeoLocalizacao(1, produto1, -8.92, -33.89);
-   	GeoLocalizacao geo2 = new GeoLocalizacao(2, produto2, -8.92, -34.89);
-   	GeoLocalizacao geo3 = new GeoLocalizacao(3, produto3, -8.12, -34.95);
-    	
-   	Duvidas duvida1 = new Duvidas(1, produto1);
-   	Duvidas duvida2 = new Duvidas(2, produto2);
-   	Duvidas duvida3 = new Duvidas(3, produto3);
-    	
-   	Anuncio anuncio1 = new Anuncio(pessoa1, produto1, new Date(), duvida1, 95.99, geo1);
-   	Anuncio anuncio2 = new Anuncio(pessoa3, produto2, new Date(), duvida2, 54.75, geo2);
-   	Anuncio anuncio3 = new Anuncio(pessoa6, produto3, new Date(), duvida3, 39.90, geo3);
-    	
-   	anuncios.criarAnuncio(anuncio1);
-   	anuncios.criarAnuncio(anuncio2);
-   	anuncios.criarAnuncio(anuncio3);
-    	
-    List<Anuncio> carrinhoDeCompras = new ArrayList<>();
+		/**
+		 * 	ANÚNCIO:
+		 * 
+		 *  - [] Criar anúncios: é preciso criar Datas, Geolocalização, Dúvidas
+		 *  
+		 */
+
+		GeoLocalizacao geo1 = new GeoLocalizacao(1, produto1, -8.92, -33.89);
+		GeoLocalizacao geo2 = new GeoLocalizacao(2, produto2, -8.92, -34.89);
+		GeoLocalizacao geo3 = new GeoLocalizacao(3, produto3, -8.12, -34.95);
+
+		Duvidas duvida1 = new Duvidas(1, produto1);
+		Duvidas duvida2 = new Duvidas(2, produto2);
+		Duvidas duvida3 = new Duvidas(3, produto3);
+
+		Anuncio anuncio1 = new Anuncio(pessoa1, produto1, new Date(), duvida1, 95.99, geo1);
+		Anuncio anuncio2 = new Anuncio(pessoa3, produto2, new Date(), duvida2, 54.75, geo2);
+		Anuncio anuncio3 = new Anuncio(pessoa6, produto3, new Date(), duvida3, 39.90, geo3);
+
+		anuncios.criarAnuncio(anuncio1);
+		anuncios.criarAnuncio(anuncio2);
+		anuncios.criarAnuncio(anuncio3);
+
+		List<Anuncio> carrinhoDeCompras = new ArrayList<>();
 
 		// [fix] source: Aula 18/04, 9-12h:
 		// https://github.com/alvesmog/sprintCodeEcommerce/commit/b069ece92b571a188769eec075737660e8a44743
@@ -145,8 +149,8 @@ public class App
 
 		InterfaceSubMenuPessoas menuPessoas = new InterfaceSubMenuPessoas();
 		InterfaceSubMenuProdutos menuProdutos = new InterfaceSubMenuProdutos();
-    InterfaceSubMenuVenda menuVendas = new InterfaceSubMenuVenda();
-		
+		InterfaceSubMenuVenda menuVendas = new InterfaceSubMenuVenda();
+
 		int rep;
 		do {
 			rep = menuGeral.mostrarMenuOpcoes();
@@ -160,16 +164,16 @@ public class App
 				} while (op != 0);
 				break;
 			case 2 : //chamar a classe/metodo do submenu de gestão de produtos 		
- 				Integer opcao;
-  				do {
- 					opcao = menuProdutos.mostrarSubMenuProdutos();
- 					opcao = menuProdutos.ingressaOpcoesGestaoProdutos(opcao, produtos, categorias);
-  				} while (!opcao.equals(0));
+				Integer opcao;
+				do {
+					opcao = menuProdutos.mostrarSubMenuProdutos();
+					opcao = menuProdutos.ingressaOpcoesGestaoProdutos(opcao, produtos, categorias);
+				} while (!opcao.equals(0));
 			case 3: // chamar a classe/metodo do submenu de gestão de anuncio
 				break;
 			case 4: // chamar a classe/metodo do submenu de gestão de vendas
-        Integer opcao;
- 				do {
+				Integer opcao;
+				do {
 					opcao = menuVendas.mostrarSubMenuVendas();
 					// @TODO: Organizar a interface `RepositorioAnuncio` para não precisar do 'cast'
 					opcao = menuVendas.ingressaOpcoesParaVenda(
@@ -180,8 +184,8 @@ public class App
 							financas, 
 							carrinhoDeCompras, 
 							controladorSessao);
-					
- 				} while (!opcao.equals(0));
+
+				} while (!opcao.equals(0));
 				break;
 			case 5: // chamar a classe/metodo do submenu de gestão de atendimento
 				break;
